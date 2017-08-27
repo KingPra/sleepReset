4106a9e86be92460e72b953ca49d160c76827421
KingPra <kingi.pra@gmail.com>
2017-08-27 12:11:48 -0400

    js functions

--- docs/app.js
+++ docs/app.js
@@ -1,2 +1,22 @@
 console.log('hello');
 
+function setDate() {
+  const now = new Date ();
+
+const secondHand = document.querySelector('.sec');
+const seconds = now.getSeconds();
+const secDegrees = ((seconds / 60) * 360) + 90;
+secondHand.style.transform = (`rotate(${secDegrees}deg)`)
+
+const minHand = document.querySelector('.min');
+const min = now.getMinutes();
+const minDegrees = ((min / 60) * 360) + 90;
+minHand.style.transform = (`rotate(${minDegrees}deg)`);
+
+const hourHand = document.querySelector('.hour');
+const hour = now.getHours();
+const hourDegree = ((hour / 12) * 360) + 90;
+hourHand.style.transform = (`rotate(${hourDegree}deg)`);
+};
+
+setInterval(setDate, 1000)
