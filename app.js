be4921ab5be740c1d71a1c120496e8ab32f11492
KingPra <kingi.pra@gmail.com>
2017-08-26 21:02:34 -0400

    initial setup

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('hello');
+
+// function setDate() {
+//   const now = new Date ();
+// }
+//
+// const secondHand = document.querySelector('sec');
+//
+// const seconds = now.getSeconds();
