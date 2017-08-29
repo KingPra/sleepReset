0d276c2900bfddeefc778799dc9f90ea90d1626e
KingPra <kingi.pra@gmail.com>
2017-08-29 18:52:10 -0400

    starting timer function

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
-console.log('hello');
+
+const audio = new Audio('sounds/Alarm-tone.mp3')
+const now = new Date ();
 
 function setDate() {
-  const now = new Date ();
+  const secondHand = document.querySelector('.sec');
+  const seconds = now.getSeconds();
+  const secDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = (`rotate(${secDegrees}deg)`)
 
-const secondHand = document.querySelector('.sec');
-const seconds = now.getSeconds();
-const secDegrees = ((seconds / 60) * 360) + 90;
-secondHand.style.transform = (`rotate(${secDegrees}deg)`)
+  const minHand = document.querySelector('.min');
+  const min = now.getMinutes();
+  const minDegrees = ((min / 60) * 360) + 90;
+  minHand.style.transform = (`rotate(${minDegrees}deg)`);
 
-const minHand = document.querySelector('.min');
-const min = now.getMinutes();
-const minDegrees = ((min / 60) * 360) + 90;
-minHand.style.transform = (`rotate(${minDegrees}deg)`);
-
-const hourHand = document.querySelector('.hour');
-const hour = now.getHours();
-const hourDegree = ((hour / 12) * 360) + 90;
-hourHand.style.transform = (`rotate(${hourDegree}deg)`);
+  const hourHand = document.querySelector('.hour');
+  const hour = now.getHours();
+  const hourDegree = ((hour / 12) * 360) + 90;
+  hourHand.style.transform = (`rotate(${hourDegree}deg)`);
 };
 
 setInterval(setDate, 1000)
+
+function buttonClick () {
+  console.log('clicky');
+  let el = document.querySelector('.start_end')
+  audio.play();
+
+  if (el.innerHTML == 'Start') {
+    el.innerHTML = 'Stop';
+    console.log(now.getMinutes());
+
+  } else {
+    el.innerHTML = 'Start';
+  }
+}
+
+function snooze () {
+  console.log('mo sleep');
+  audio.pause();
+  audio.currentTime = 0;
+}
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const browser = require('gulp-browser');
 const strip = require('gulp-strip-comments');
 
-gulp.task('default', ['html', 'css', 'js']);
+gulp.task('default', ['html', 'css', 'js', 'sounds']);
 
 gulp.task('html', () => {
   gulp.src('*.html')
@@ -24,8 +24,14 @@ gulp.task('js', () => {
   .pipe(gulp.dest('docs/'))
 });
 
+gulp.task('sounds', () => {
+  gulp.src('sounds/*.mp3')
+  .pipe(gulp.dest('docs/sounds'))
+});
+
 gulp.task('watch', ['default'], () => {
   gulp.watch('*.html', ['html']);
   gulp.watch('*.scss', ['css']);
   gulp.watch('*.js', ['js']);
+  gulp.watch('sounds/*.mp3', ['sounds']);
 });
